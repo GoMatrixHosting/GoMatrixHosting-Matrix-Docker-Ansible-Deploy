2c5aec577d9e8a288f00dcd9d561ea867cf1a711
Michael-GMH <gomatrixhosting@tutanota.com>
2021-05-31 15:11:25 +0800

    add testing debug

--- autofarm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd ~/chia-blockchain
-. ./activate
-chia init
-chia start harvester
-chia start farmer
--- chiashow.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd ~/chia-blockchain
-. ./activate
-#chia init
-chia show -s
-chia farm summary
-chia wallet show
-exit
